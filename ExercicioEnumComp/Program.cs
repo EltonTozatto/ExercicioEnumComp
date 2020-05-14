96c695e7df0bbf3a2790136e900e5fe7d08be3cd
Elton Tozatto <eltontozatto@gmail.com>
2020-05-14 11:02:37 -0300

    Execucao da aula de Enumeracoes e composicao

---
+++ ExercicioEnumComp/Program.cs
@@ -0,0 +1,47 @@
+﻿using ExercicioEnumComp.Entities;
+using ExercicioEnumComp.Entities.Enums;
+using System;
+using System.Globalization;
+
+namespace ExercicioEnumComp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter client data: ");
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Email: ");
+            string email = Console.ReadLine();
+            Console.Write("Birth date (DD/MM/YYYY): ");
+            DateTime date = DateTime.Parse(Console.ReadLine());
+            Client client = new Client(name, email, date);
+
+            Console.WriteLine("Enter order data: ");
+            Console.Write("Status: ");
+            OrderStatus status = Enum.Parse<OrderStatus>(Console.ReadLine());
+            Order order = new Order(DateTime.Now, status, client);
+
+            Console.Write("How many items to this order? ");
+            int numItems = int.Parse(Console.ReadLine());
+            for(int i = 1; i <= numItems; i++)
+            {
+                Console.WriteLine($"Enter #{i} item data:");
+                Console.Write("Product name: ");
+                string productName = Console.ReadLine();
+                Console.Write("Product price: ");
+                double productPrice = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Product product = new Product(productName, productPrice);
+                Console.Write("Quantity: ");
+                int quantity = int.Parse(Console.ReadLine());
+                OrderItem orderItem = new OrderItem(quantity, product.Price, product);
+                order.AddItem(orderItem);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("ORDER SUMMARY: ");
+            Console.WriteLine(order.ToString());
+        }
+    }
+}
